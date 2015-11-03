aae37de59f462f840c318147b5f69d3e42cdfdc7
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2015-11-03 18:48:10 +0200

    index upd

--- site/js/app.js
+++ site/js/app.js
@@ -27,7 +27,16 @@ $(document).ready(function() {
 	var logoSlider = $(".js-logo-slider");
 	var logoSliderOpt = {
 		slidesToShow: 5,
-		slidesToScroll: 5
+		slidesToScroll: 5,
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	}
 	logoSlider.slick(logoSliderOpt);
 
